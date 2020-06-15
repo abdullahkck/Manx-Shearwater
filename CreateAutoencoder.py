c3168195c7b3e9bc3b471e169cdc1b41e352af3b
abdullah <abdullahkucuk1991@gmail.com>
2020-06-15 18:43:42 +0100

    Extracting Encoder

--- CreateAutoencoder.py
+++ CreateAutoencoder.py
@@ -20,9 +20,11 @@ y = pickle.load(pickle_in)
 
 X_train, X_test, _, _ = train_test_split(X, y, test_size=0.2, random_state=42)
 
+X = np.array(X)
 X_train = np.array(X_train)
 X_test = np.array(X_test)
 
+X = X/255.0
 X_train = X_train/255.0
 X_test = X_test/255.0
 
@@ -35,7 +37,7 @@ x = MaxPooling2D((2, 2), padding='same')(x)
 x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
 x = MaxPooling2D((2, 2), padding='same')(x)
 x = Conv2D(4, (3, 3), activation='relu', padding='same')(x)
-encoded = MaxPooling2D((2, 2), padding='same')(x)
+encoded = MaxPooling2D((2, 2), padding='same', name='encoded')(x)
 
 x = Conv2D(4, (3, 3), activation='relu', padding='same')(encoded)
 x = UpSampling2D((2, 2))(x)
@@ -55,22 +57,64 @@ autoencoder.fit(X_train, X_train,
                 batch_size=32,
                 shuffle=True,
                 validation_data=(X_test, X_test),
-                callbacks=[TensorBoard(log_dir='logs/autoencoder')])
+                callbacks=[TensorBoard(log_dir='logs/full_autoencoder_e30')])
+
+# -----------------------------------------------
+# Get the encoder layer
+encoder = Model(inputs=autoencoder.input, outputs=autoencoder.get_layer('encoded').output)
+# Get the encoded inputs for unsupervised learning
+X_encoded_test = encoder.predict(X_test)  # Just to plot
+X_encoded = encoder.predict(X)
+
+# Save encoded inputs for unsupervised learning
+pickle_out = open("X_encoded.pickle", "wb")
+pickle.dump(X_encoded, pickle_out)
+pickle_out.close()
+# -----------------------------------------------
 
 decoded_imgs = autoencoder.predict(X_test)
 
+m = 6
 n = 5
-plt.figure(figsize=(20, 4))
+plt.figure(figsize=(20, 6))
 for i in range(n):
     # display original
-    ax = plt.subplot(2, n, i + 1)
+    ax = plt.subplot(m, n, i + 1 + (n * 0))
     plt.imshow(X_test[i].reshape(480, 640))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
+    # display encoded conv 1
+    ax = plt.subplot(m, n, i + 1 + (n * 1))
+    plt.imshow(X_encoded_test[i].reshape(30, 40, 4)[:, :, 0])
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display encoded conv 2
+    ax = plt.subplot(m, n, i + 1 + (n * 2))
+    plt.imshow(X_encoded_test[i].reshape(30, 40, 4)[:, :, 1])
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display encoded conv 3
+    ax = plt.subplot(m, n, i + 1 + (n * 3))
+    plt.imshow(X_encoded_test[i].reshape(30, 40, 4)[:, :, 2])
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    # display encoded conv 4
+    ax = plt.subplot(m, n, i + 1 + (n * 4))
+    plt.imshow(X_encoded_test[i].reshape(30, 40, 4)[:, :, 3])
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
     # display reconstruction
-    ax = plt.subplot(2, n, i + 1 + n)
+    ax = plt.subplot(m, n, i + 1 + (n * 5))
     plt.imshow(decoded_imgs[i].reshape(480, 640))
     plt.gray()
     ax.get_xaxis().set_visible(False)
