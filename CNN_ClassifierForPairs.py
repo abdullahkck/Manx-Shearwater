4f66aa48a150662d55f00aaf6942472b316d72fe
abdullah <abdullahkucuk1991@gmail.com>
2020-07-05 19:22:08 +0100

    CNN Classifier for Pairs

---
+++ CNN_ClassifierForPairs.py
@@ -0,0 +1,178 @@
+import tensorflow as tf
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+import os
+import pickle
+from keras.callbacks import TensorBoard
+from datetime import datetime
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+from sklearn.model_selection import KFold
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import confusion_matrix
+
+
+num_folds = 10
+fold_no = 1
+epoch = 25
+NAME = "Pairs-(685_754)-(686_755)-(686_308)-" + str(num_folds) + "-folds-" + str(epoch) + "-epochs"
+num_classes = 3
+acc_per_fold = []
+loss_per_fold = []
+macro_auc_per_fold = []
+weighted_auc_per_fold = []
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'True'
+
+pickle_in = open("Calls_67_pairs_X.pickle", "rb")
+#  pickle_in = open("X.pickle", "rb")
+X = pickle.load(pickle_in)
+
+pickle_in = open("Calls_67_pairs_y.pickle", "rb")
+#  pickle_in = open("y.pickle", "rb")
+y = pickle.load(pickle_in)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+X_train = X_train/255.0
+X_test = X_test/255.0
+
+inputs = np.concatenate((X_train, X_test), axis=0)
+targets = np.concatenate((y_train, y_test), axis=0)
+
+# convert the training labels to categorical vectors
+targets = to_categorical(targets, num_classes=3)
+
+k_fold = KFold(n_splits=num_folds, shuffle=True)
+
+print(".....Training started.....")
+start_time = datetime.now()
+
+for train, test in k_fold.split(inputs, targets):
+    print("Current Fold: ", fold_no)
+    model = Sequential()
+
+    model.add(Conv2D(16, (3, 3), input_shape=(640, 480, 1), padding='same'))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+
+    model.add(Conv2D(16, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(3, 3)))
+
+    model.add(Conv2D(16, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(3, 1)))
+
+    model.add(Conv2D(16, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(3, 1)))
+
+    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+
+    model.add(Dense(256))
+    model.add(Dense(32))
+    model.add(Dense(3))
+
+    model.add(Activation('sigmoid'))
+
+    # model.summary()
+
+    tensorboard = TensorBoard(log_dir="logs/{}".format(NAME))
+
+    model.compile(loss='binary_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+
+    model.fit(inputs[train], targets[train], batch_size=128, epochs=epoch, validation_split=0.2, callbacks=[tensorboard])
+
+    # Test vs Training
+    test_eval = model.evaluate(inputs[test], targets[test], verbose=0)
+    acc_per_fold.append(test_eval[1])
+    loss_per_fold.append(test_eval[0])
+
+    print('Test accuracy:', test_eval[1])
+    print('Test loss:', test_eval[0])
+
+    y_prob = model.predict_proba(inputs[test])
+    y_pred = model.predict_classes(inputs[test])
+    # print("Test Predictions:")
+    # print(y_prob)
+    # print(y_pred)
+
+    rounded_targets = np.argmax(targets[test], axis=1)
+
+    cnf_matrix = confusion_matrix(rounded_targets, y_pred, labels=[0, 1, 2])
+    # np.set_printoptions(precision=2)
+    print("Current Fold:")
+    print(fold_no)
+    print(cnf_matrix)
+
+    # plot_confusion_matrix(cm=cnf_matrix,
+    #                       normalize=True,
+    #                       target_names=['685_754', '686_755', '686_308'],
+    #                       title="Confusion Matrix " + NAME)
+
+    # for i in num_classes:
+    #   roc_curve(targets[test][:, i], y_prob[:, 0])
+    macro_roc_auc    = roc_auc_score(targets[test], y_prob, average="macro")
+    weighted_roc_auc = roc_auc_score(targets[test], y_prob, average="weighted")
+
+    macro_auc_per_fold.append(macro_roc_auc)
+    weighted_auc_per_fold.append(weighted_roc_auc)
+
+    print("ROC AUC scores:\n{:.3f} (macro),\n{:.3f} "
+          "(weighted by prevalence)"
+          .format(macro_roc_auc, weighted_roc_auc))
+
+    fold_no = fold_no + 1
+
+print('------------------------------------------------------------------------')
+time_dif = datetime.now() - start_time
+print(".....Training finished.....")
+print("Epochs: ", epoch)
+print("Folds: ", num_folds)
+print(NAME)
+print("Training time: ", time_dif)
+print('Macro AUC scores:')
+for i in range(num_folds):
+    print('Fold ' + str(i) + ': ', '%0.3f' % macro_auc_per_fold[i])
+print('Weighted AUC scores:')
+for i in range(num_folds):
+    print('Fold ' + str(i) + ': ', '%0.3f' % weighted_auc_per_fold[i])
+print('Average scores for all folds:')
+print('Accuracy:', '%0.3f' % np.mean(acc_per_fold))
+print('Std:', '%0.3f' % np.std(acc_per_fold))
+print('Loss:', '%0.3f' % np.mean(loss_per_fold))
+print('Macro AUC:', '%0.3f' % np.mean(macro_auc_per_fold))
+print('Weighted AUC:', '%0.3f' % np.mean(weighted_auc_per_fold))
+print('------------------------------------------------------------------------')
+
+
+# fig = plt.figure(figsize=(8, 6))
+# ax = fig.add_subplot(111)
+# plt.figure(1)
+# plt.plot([0, 1], [0, 1], 'k--')
+# for i in range(num_folds):
+#     plt.plot(fpr_per_fold[i], tpr_per_fold[i])
+#
+# ax.text(0.5, 0.01, 'Mean of AUCs = ' + '%0.3f' % np.mean(auc_per_fold), style='italic',
+#         bbox={'facecolor': 'red', 'alpha': 0.5, 'pad': 10})
+# plt.xlabel('False positive rate')
+# plt.ylabel('True positive rate')
+# plt.title(NAME + ' ROC curves')
+# plt.legend(loc='best')
+# plt.show()
+# fig.savefig(NAME + ".png")
+
---
+++ CreateTrainingDataForPairs.py
@@ -0,0 +1,70 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+from tqdm import tqdm
+import random
+import pickle
+
+path = '/Users/abdullahkucuk/Desktop/MSc/MSc Project/records.nosync/calls_67/Spectrograms_pairs'
+training_data = []
+categories = ["685_754", "686_755", "686_308"]
+
+
+def check_call_is_existed(img_array):
+    if np.sum(img_array[0:480, 0:640]) > 13500000:  # if the sum of pixels less than 15 millions, there should be a call
+        return True
+    return False
+
+
+def create_training_data():
+    eliminated_spec_count = 0
+    for img in tqdm(os.listdir(path)):  # iterate over each image
+        try:
+            category = img[:7]  # get the category name
+            # if category == "B007":
+            #     continue
+            class_num = categories.index(category)  # get the classification
+            img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)  # convert to array
+            if check_call_is_existed(img_array):
+                training_data.append([img_array, class_num])  # add this to our training_data
+            else:
+                eliminated_spec_count = eliminated_spec_count + 1
+                # print("Eliminated img name: ", img, " sum: ", np.sum(img_array[0:480, 0:640]))
+        except Exception as e:
+            print("Image read exception: ", e, os.path.join(path, img))
+            pass
+    print("Training data count: ", len(training_data))
+    print("Eliminated data count: ", eliminated_spec_count)
+
+
+def create_pickles():
+    X = []
+    y = []
+
+    for features, label in training_data:
+        X.append(features)
+        y.append(label)
+
+    X = np.array(X).reshape(-1, 640, 480, 1)
+
+    #  pickle_out = open("Calls_67_X_B006.pickle", "wb")
+    pickle_out = open("Calls_67_pairs_X.pickle", "wb")
+    pickle.dump(X, pickle_out)
+    pickle_out.close()
+
+    #  pickle_out = open("Calls_67_y_B006.pickle", "wb")
+    pickle_out = open("Calls_67_pairs_y.pickle", "wb")
+    pickle.dump(y, pickle_out)
+    pickle_out.close()
+
+
+def main():
+
+    create_training_data()
+    random.shuffle(training_data)
+    create_pickles()
+
+
+main()
+
